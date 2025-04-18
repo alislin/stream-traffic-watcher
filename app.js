47f4f3d31cf6b89b715dc7771ece06bdec037160
林亚 <Alis.Lin@outlook.com>
2025-04-18 16:22:00 +0800

    server.js 调整为导出模块

---
+++ app.js
@@ -0,0 +1,3 @@
+import { flowServer } from "./server.js";
+
+flowServer();
--- server.js
+++ server.js
@@ -7,90 +7,91 @@ import { WebSocketServer } from "ws";
 import { fileURLToPath } from "url";
 import { flowCheck } from "./flowCheck.js";
 
-const hostname = "127.0.0.1";
-const port = 23110;
-const wsPort = port + 1; // WebSocket 端口
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const directoryToServe = __dirname; // 服务当前目录
-const appFile =
-  process.env.NODE_ENV === "production" ? "flowCheck.cjs" : "flowCheck.js";
+export function flowServer(opt) {
+  const hostname = "127.0.0.1";
+  const port = opt?.port ?? 23110;
+  const wsPort = opt?.port ?? port + 1; // WebSocket 端口
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const directoryToServe = __dirname; // 服务当前目录
+  const appFile =
+    process.env.NODE_ENV === "production" ? "flowCheck.cjs" : "flowCheck.js";
 
-function runAppJS() {
-  console.log("Running flowCheck at startup...");
-  flowCheck({ file: "url.txt", onFinish: () => broadcast("reload") });
-  return;
-}
-
-
-// 创建 WebSocket 服务器
-const wss = new WebSocketServer({ port: wsPort });
-
-wss.on("connection", (ws) => {
-  console.log("Client connected");
-  ws.on("close", () => console.log("Client disconnected"));
-});
-
-function broadcast(message) {
-  wss.clients.forEach((client) => {
-    client.send(message);
-  });
-}
-
-const server = http.createServer((req, res) => {
-  let filePath = path.join(
-    directoryToServe,
-    req.url === "/" ? "index.html" : req.url
-  );
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        // 文件未找到
-        res.writeHead(404);
-        res.end("404 Not Found");
-      } else {
-        // 服务器错误
-        res.writeHead(500);
-        res.end("500 Internal Server Error: " + err.code);
-      }
-    } else {
-      // 成功读取文件
-      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-      res.end(content);
-    }
-  });
-});
-
-function getContentType(filePath) {
-  let extname = path.extname(filePath);
-  switch (extname) {
-    case ".html":
-      return "text/html";
-    case ".js":
-      return "text/javascript";
-    case ".css":
-      return "text/css";
-    case ".json":
-      return "application/json";
-    case ".png":
-      return "image/png";
-    case ".jpg":
-      return "image/jpg";
-    default:
-      return "application/octet-stream";
+  function runAppJS() {
+    console.log("Running flowCheck at startup...");
+    flowCheck({ file: "url.txt", onFinish: () => broadcast("reload") });
+    return;
   }
-}
 
-// 每 1 分钟执行一次 flowCheck.js (测试用)
-cron.schedule("*/1 * * * *", () => {
+  // 创建 WebSocket 服务器
+  const wss = new WebSocketServer({ port: wsPort });
+
+  wss.on("connection", (ws) => {
+    console.log("Client connected");
+    ws.on("close", () => console.log("Client disconnected"));
+  });
+
+  function broadcast(message) {
+    wss.clients.forEach((client) => {
+      client.send(message);
+    });
+  }
+
+  const server = http.createServer((req, res) => {
+    let filePath = path.join(
+      directoryToServe,
+      req.url === "/" ? "index.html" : req.url
+    );
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        if (err.code === "ENOENT") {
+          // 文件未找到
+          res.writeHead(404);
+          res.end("404 Not Found");
+        } else {
+          // 服务器错误
+          res.writeHead(500);
+          res.end("500 Internal Server Error: " + err.code);
+        }
+      } else {
+        // 成功读取文件
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.end(content);
+      }
+    });
+  });
+
+  function getContentType(filePath) {
+    let extname = path.extname(filePath);
+    switch (extname) {
+      case ".html":
+        return "text/html";
+      case ".js":
+        return "text/javascript";
+      case ".css":
+        return "text/css";
+      case ".json":
+        return "application/json";
+      case ".png":
+        return "image/png";
+      case ".jpg":
+        return "image/jpg";
+      default:
+        return "application/octet-stream";
+    }
+  }
+
+  // 每 1 分钟执行一次 flowCheck.js (测试用)
+  cron.schedule("*/1 * * * *", () => {
+    runAppJS();
+    return;
+  });
+
   runAppJS();
-  return;
-});
 
-runAppJS();
-
-server.listen(port, hostname, () => {
-  const serviceUrl = `http://${hostname}:${port}/`;
-  console.log(`HTTP Server running at ${serviceUrl}`);
-  console.log(`WebSocket Server running at ws://${hostname}:${wsPort}`);
-});
+  server.listen(port, hostname, () => {
+    const serviceUrl = `http://${hostname}:${port}/`;
+    console.log(`HTTP Server running at ${serviceUrl}`);
+    console.log(`WebSocket Server running at ws://${hostname}:${wsPort}`);
+  });
+}
